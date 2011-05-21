92f6f76517b76eb189763e63f4d12faca42272eb
Akash Manohar J <akash@akash.im>
2011-05-21 16:39:46 +0530

    have clean grammar file + node methods
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ nodes.rb
@@ -0,0 +1,68 @@
+module Ennio
+
+    class Program < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+    
+    class SExpression < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class Operator < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class ArithmeticOperator < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class LogicalOperator < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class RelationalOperator < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class Value < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class IntegerNode < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class FloatNode < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+    
+    class DoubleQuotedString < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class SingleQuoteedString < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class List < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+
+    class Space < Treetop::Runtime::SyntaxNode
+        def node_value
+        end
+    end
+end
+
